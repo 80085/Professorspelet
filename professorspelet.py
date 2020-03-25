68f862a7728f097ea0b40e322550bdad5cde0f04
80085 <6232591+80085@users.noreply.github.com>
2020-03-25 14:37:30 +0100

    Add source files

---
+++ professorspelet.py
@@ -0,0 +1,166 @@
+import copy
+
+
+class Puzzle:
+    """
+        Professorspelet consists of a 4x4 grid that shall be filled with tiles,
+        each of which has 4 body parts in different colors on each edge.
+        The tiles must be place in such a way that all edges must create one
+        complete Professor.
+    """
+
+    def __init__(self, tiles: list):
+        """
+            Initialize a puzzle with a list of tiles that shall be placed. The list can be empty or
+            contain only a few Professors, but must not exceed 16.
+        """
+        if len(tiles) > 16:
+            raise ValueError(f'Maximum number of Professor tiles that can be placed is 16, was: {len(tiles)}')
+        self.grid = [None] * 16
+        self._solve(copy.deepcopy(tiles))
+
+    def solution(self):
+        """
+            Return a list of tiles placement for a valid solution. The tiles are indexed 0-15 and should be placed
+            starting from top left corner with a column width of 4.
+        """
+        return [(e, str(tile)) for e, tile in enumerate(self.grid) if tile is not None]
+
+    def _solve(self, tiles: list, current_index: int=0):
+        if not tiles:
+            return True
+        for tile in tiles:
+            for i in range(4):
+                if self._can_place_tile(tile, current_index):
+                    self.grid[current_index] = tile
+                    tiles.remove(tile)
+                    if self._solve(tiles, current_index + 1):
+                        return True
+                    self.grid[current_index] = None
+                    tiles.append(tile)
+                tile.rotate()
+        return False
+
+    def _can_place_tile(self, tile, ind):
+        if self.grid[ind] is not None:
+            raise ValueError(f'Index={ind} is not empty')
+        if ind > 3:
+            other = self.grid[ind - 4]
+            if other is not None and not other.bottom().matches(tile.top()):
+                return False
+        if ind < 12:
+            other = self.grid[ind + 4]
+            if other is not None and not other.top().matches(tile.bottom()):
+                return False
+        if ind % 4 != 0:
+            other = self.grid[ind - 1]
+            if other is not None and not other.right().matches(tile.left()):
+                return False
+        if ind % 4 != 3:
+            other = self.grid[ind + 1]
+            if other is not None and not other.left().matches(tile.right()):
+                return False
+        return True
+
+
+class Tile:
+    """
+        A tile consisting of four professor halves - two upper and two lower
+    """
+
+    def __init__(self, professors):
+        """
+            Create a card with four professors, starting at the top in a clockwise direction
+        """
+        if len(professors) != 4:
+            raise ValueError('Expected four professor parts')
+        bodies = list(map(lambda x: x.body, professors))
+        if bodies.count('upper') != bodies.count('lower'):
+            raise ValueError('Expected equal number of upper and lower body parts')
+        self.professors = professors
+        self.rotation = 0
+
+    def __str__(self):
+        return f'Top: {self.top()}, Right: {self.right()}, Bottom: {self.bottom()}, Left: {self.left()}'
+
+    def top(self):
+        """
+            Get the professor in top position
+        """
+        return self.professors[self.rotation % 4]
+
+    def right(self):
+        """
+            Get the professor in right position
+        """
+        return self.professors[(self.rotation + 1) % 4]
+
+    def bottom(self):
+        """
+            Get the professor in bottom position
+        """
+        return self.professors[(self.rotation + 2) % 4]
+
+    def left(self):
+        """
+            Get the professor in left position
+        """
+        return self.professors[(self.rotation + 3) % 4]
+
+    def rotate(self, steps=1) -> None:
+        """
+            Rotate this tile a number of steps clockwise.
+            Number may be negative to indicate counter clockwise.
+        """
+        self.rotation -= steps
+
+
+class Professor:
+    """
+        Representation of a professor who consists of a body half in a particular color
+    """
+
+    _VALID_BODY_PARTS = ['upper', 'lower']
+    _VALID_COLORS = ['blue', 'purple', 'green', 'brown']
+
+    def __init__(self, body, color):
+        if body.lower() not in Professor._VALID_BODY_PARTS:
+            raise ValueError(f'Invalid body part {body}. Valid alternatives: {Professor._VALID_BODY_PARTS}')
+        if color.lower() not in Professor._VALID_COLORS:
+            raise ValueError(f'Invalid color {color}. Valid alternatives: {Professor._VALID_COLORS}')
+        self.body = body.lower()
+        self.color = color.lower()
+
+    def __str__(self):
+        return f'{self.body} - {self.color}'
+
+    def matches(self, professor) -> bool:
+        """
+            Test if a professor can be placed next to another one
+            by comparing body parts and color.
+        """
+        return self.body != professor.body and self.color == professor.color
+
+
+if __name__ == '__main__':
+    tiles = [
+        Tile([Professor('lower', 'green'), Professor('lower', 'brown'), Professor('upper', 'purple'), Professor('upper', 'blue')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'green'), Professor('upper', 'blue'), Professor('upper', 'purple')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'purple'), Professor('upper', 'green'), Professor('upper', 'brown')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'purple')]),
+        Tile([Professor('lower', 'blue'), Professor('lower', 'green'), Professor('upper', 'brown'), Professor('upper', 'purple')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'purple'), Professor('upper', 'green'), Professor('upper', 'blue')]),
+        Tile([Professor('lower', 'green'), Professor('lower', 'blue'), Professor('upper', 'green'), Professor('upper', 'purple')]),
+        Tile([Professor('lower', 'green'), Professor('lower', 'blue'), Professor('upper', 'brown'), Professor('upper', 'purple')]),
+        Tile([Professor('lower', 'blue'), Professor('lower', 'brown'), Professor('upper', 'brown'), Professor('upper', 'blue')]),
+        Tile([Professor('lower', 'green'), Professor('lower', 'purple'), Professor('upper', 'blue'), Professor('upper', 'brown')]),
+        Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'brown'), Professor('upper', 'purple')]),
+        Tile([Professor('lower', 'green'), Professor('lower', 'purple'), Professor('upper', 'brown'), Professor('upper', 'blue')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'purple'), Professor('upper', 'green'), Professor('upper', 'blue')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'blue'), Professor('upper', 'green'), Professor('upper', 'green')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'purple'), Professor('upper', 'brown'), Professor('upper', 'blue')]),
+        Tile([Professor('lower', 'brown'), Professor('lower', 'blue'), Professor('upper', 'purple'), Professor('upper', 'green')])
+    ]
+    puzzle = Puzzle(tiles)
+    for s in puzzle.solution():
+        print(*s)
---
+++ test_professorspelet.py
@@ -0,0 +1,171 @@
+import unittest
+from professorspelet import Professor, Tile, Puzzle
+
+
+class TestProfessor(unittest.TestCase):
+    def test_create_is_case_insensitve(self):
+        professor = Professor('Upper', 'Blue')
+        self.assertEqual(professor.body, 'upper')
+        self.assertEqual(professor.color, 'blue')
+
+    def test_create_raises_for_invalid_body_part(self):
+        with(self.assertRaisesRegex(ValueError, 'body')):
+            Professor('middle', 'blue')
+
+    def test_create_raises_for_invalid_color(self):
+        with(self.assertRaisesRegex(ValueError, 'color')):
+            Professor('upper', 'red')
+
+    def test_has_a_meaningful_str_representation(self):
+        professor = Professor('Upper', 'Blue')
+        self.assertEqual(str(professor), 'upper - blue')
+
+    def test_matches(self):
+        first = Professor('upper', 'blue')
+        second = Professor('lower', 'blue')
+        self.assertTrue(first.matches(second))
+
+    def test_matches_body_part_mismatch(self):
+        first = Professor('lower', 'blue')
+        second = Professor('lower', 'blue')
+        self.assertFalse(first.matches(second))
+
+    def test_matches_color_mismatch(self):
+        first = Professor('upper', 'purple')
+        second = Professor('lower', 'blue')
+        self.assertFalse(first.matches(second))
+
+
+class TestTile(unittest.TestCase):
+    def test_create_too_few_professors(self):
+        professor = Professor('lower', 'green')
+        with(self.assertRaisesRegex(ValueError, 'Expected four professor')):
+            Tile([professor] * 3)
+
+    def test_create_too_many_professors(self):
+        professor = Professor('lower', 'purple')
+        with(self.assertRaisesRegex(ValueError, 'Expected four professor')):
+            Tile([professor] * 5)
+
+    def test_create_not_equal_number_of_upper_and_lower_body(self):
+        professor = Professor('lower', 'purple')
+        with(self.assertRaisesRegex(ValueError, 'Expected equal number of upper and lower body parts')):
+            Tile([professor] * 4)
+
+    def test_stores_correct_professor_in_correct_place(self):
+        professors = [Professor('lower', 'purple'), Professor('lower', 'blue'), Professor('upper', 'green'), Professor('upper', 'brown')]
+        tile = Tile(professors)
+        self.assertEqual(tile.top(), professors[0])
+        self.assertEqual(tile.right(), professors[1])
+        self.assertEqual(tile.bottom(), professors[2])
+        self.assertEqual(tile.left(), professors[3])
+
+    def test_rotate_clockwise(self):
+        professors = [Professor('lower', 'purple'), Professor('lower', 'blue'), Professor('upper', 'green'), Professor('upper', 'brown')]
+        tile = Tile(professors)
+        tile.rotate()
+        self.assertEqual(tile.top(), professors[3])
+        self.assertEqual(tile.right(), professors[0])
+        self.assertEqual(tile.bottom(), professors[1])
+        self.assertEqual(tile.left(), professors[2])
+
+    def test_rotate_counter_clockwise(self):
+        professors = [Professor('lower', 'purple'), Professor('lower', 'blue'), Professor('upper', 'green'), Professor('upper', 'brown')]
+        tile = Tile(professors)
+        tile.rotate(-1)
+        self.assertEqual(tile.top(), professors[1])
+        self.assertEqual(tile.right(), professors[2])
+        self.assertEqual(tile.bottom(), professors[3])
+        self.assertEqual(tile.left(), professors[0])
+
+    def test_tile_has_a_meaningful_str_representation(self):
+        professors = [Professor('lower', 'purple'), Professor('lower', 'blue'), Professor('upper', 'green'), Professor('upper', 'brown')]
+        tile_str = str(Tile(professors))
+        for p in professors:
+            self.assertTrue(str(p) in tile_str, f'Expected {p} to be in tile string representation')
+
+
+class TestPuzzle(unittest.TestCase):
+    def test_constructor_raises_when_too_many_tiles_given(self):
+        tiles = [Tile([Professor('lower', 'green')] * 2 + [Professor('upper', 'green')] * 2)] * 17
+        with(self.assertRaisesRegex(ValueError, 'Maximum number of Professor tiles that can be placed is 16, was: 17')):
+            Puzzle(tiles)
+
+    def test_can_place_tile_when_grid_is_empty(self):
+        tile = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        puzzle = Puzzle([])
+        self.assertTrue(puzzle._can_place_tile(tile, 0))
+
+    def test_can_place_tile_raises_when_position_is_occupied(self):
+        tile = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        puzzle = Puzzle([])
+        puzzle.grid[0] = tile
+        with(self.assertRaisesRegex(ValueError, 'Index.*is not empty')):
+            puzzle._can_place_tile(tile, 0)
+
+    def test_can_place_tile_true_when_match(self):
+        first_tile = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        second_tile = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        puzzle = Puzzle([])
+        puzzle.grid[0] = first_tile
+        self.assertTrue(puzzle._can_place_tile(second_tile, 1))
+
+    def test_can_place_tile_false_when_body_mismatch(self):
+        first_tile = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        second_tile = Tile([Professor('upper', 'green'), Professor('upper', 'green'), Professor('lower', 'green'), Professor('lower', 'green')])
+        puzzle = Puzzle([])
+        puzzle.grid[0] = first_tile
+        self.assertFalse(puzzle._can_place_tile(second_tile, 1))
+
+    def test_can_place_tile_false_when_color_mismatch(self):
+        all_green_tile = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        all_blue_tile = Tile([Professor('lower', 'blue'), Professor('lower', 'blue'), Professor('upper', 'blue'), Professor('upper', 'blue')])
+        puzzle = Puzzle([])
+        puzzle.grid[0] = all_green_tile
+        self.assertFalse(puzzle._can_place_tile(all_blue_tile, 1))
+
+    def test_can_place_tile_if_all_neighboring_tiles_match(self):
+        puzzle = Puzzle([])
+        puzzle.grid[1] = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        puzzle.grid[6] = Tile([Professor('lower', 'blue'), Professor('lower', 'blue'), Professor('upper', 'blue'), Professor('upper', 'blue')])
+        puzzle.grid[9] = Tile([Professor('lower', 'purple'), Professor('lower', 'purple'), Professor('upper', 'purple'), Professor('upper', 'purple')])
+        puzzle.grid[4] = Tile([Professor('lower', 'brown'), Professor('lower', 'brown'), Professor('upper', 'brown'), Professor('upper', 'brown')])
+        matching_tile = Tile([Professor('lower', 'green'), Professor('lower', 'blue'), Professor('upper', 'purple'), Professor('upper', 'brown')])
+        self.assertTrue(puzzle._can_place_tile(matching_tile, 5))
+
+    def test_can_place_tile_if_rotated_correct(self):
+        puzzle = Puzzle([])
+        puzzle.grid[1] = Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])
+        puzzle.grid[6] = Tile([Professor('lower', 'blue'), Professor('lower', 'blue'), Professor('upper', 'blue'), Professor('upper', 'blue')])
+        puzzle.grid[9] = Tile([Professor('lower', 'purple'), Professor('lower', 'purple'), Professor('upper', 'purple'), Professor('upper', 'purple')])
+        puzzle.grid[4] = Tile([Professor('lower', 'brown'), Professor('lower', 'brown'), Professor('upper', 'brown'), Professor('upper', 'brown')])
+        tile = Tile([Professor('lower', 'blue'), Professor('upper', 'purple'), Professor('upper', 'brown'), Professor('lower', 'green')])
+        self.assertFalse(puzzle._can_place_tile(tile, 5))
+        tile.rotate()
+        self.assertTrue(puzzle._can_place_tile(tile, 5))
+
+    def test_solve_two_tiles(self):
+        tiles = [Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')]),
+                 Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')])]
+        self.assertTrue(Puzzle(tiles).solution())
+
+    def test_solve_two_tiles_with_different_color_does_not_solve(self):
+        tiles = [Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'green')]),
+                 Tile([Professor('lower', 'blue'), Professor('lower', 'blue'), Professor('upper', 'blue'), Professor('upper', 'blue')])]
+        self.assertFalse(Puzzle(tiles).solution())
+
+    def test_solve_two_tiles_are_solved_when_rotate(self):
+        tiles = [Tile([Professor('lower', 'blue'), Professor('lower', 'purple'), Professor('upper', 'green'), Professor('upper', 'green')]),
+                 Tile([Professor('upper', 'green'), Professor('upper', 'blue'), Professor('lower', 'purple'), Professor('lower', 'purple')])]
+        self.assertTrue(Puzzle(tiles).solution())
+
+    def test_solve_three_tiles(self):
+        tiles = [Tile([Professor('lower', 'green'), Professor('lower', 'green'), Professor('upper', 'green'), Professor('upper', 'brown')]),
+                 Tile([Professor('lower', 'purple'), Professor('lower', 'purple'), Professor('upper', 'purple'), Professor('upper', 'purple')]),
+                 Tile([Professor('lower', 'brown'), Professor('lower', 'purple'), Professor('upper', 'purple'), Professor('upper', 'purple')])]
+        puzzle = Puzzle(tiles)
+        self.assertTrue(puzzle.solution())
+
+
+if __name__ == '__main__':
+    unittest.main()
